2a80eacab395df5b33987e5202ee0f86cbf0657f
freqai <tzchat@skiff.com>
2024-03-28 20:11:05 +0000

    add

--- rust/client/src/order.rs
+++ rust/client/src/order.rs
@@ -31,16 +31,7 @@ impl BpxClient {
         let endpoint = format!("{}/api/v1/order", self.base_url);
         let res = self.post(endpoint, payload).await?;
 
-        let text = res.text().await?;
-        // 然后尝试将文本内容解析为 JSON
-        match serde_json::from_str::<Order>(&text) {
-            Ok(order) => Ok(order), // 如果成功解析，返回 Order
-            Err(e) => {
-                println!("Failed to parse JSON: {}", e);
-                println!("Response text: {}", text);
-                Err(e.into()) // 将解析错误转换为合适的错误类型
-            }
-        }
+        res.json().await.map_err(Into::into)
     }
 
     pub async fn cancel_order(
