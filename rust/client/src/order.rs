ac8f4db3c0f4275922038c32ef7b26e3e53eeba4
freqai <tzchat@skiff.com>
2024-03-28 09:45:03 +0000

    add

---
+++ rust/client/src/order.rs
@@ -0,0 +1,72 @@
+use bpx_api_types::order::{
+    CancelOpenOrdersPayload, CancelOrderPayload, ExecuteOrderPayload, Order,
+};
+
+use crate::error::{Error, Result};
+use crate::BpxClient;
+
+impl BpxClient {
+    pub  fn get_open_order(
+        &self,
+        symbol: &str,
+        order_id: Option<&str>,
+        client_id: Option<u32>,
+    ) -> Result<Order> {
+        let mut url = format!("{}/api/v1/order?symbol={}", self.base_url, symbol);
+        if let Some(order_id) = order_id {
+            url.push_str(&format!("&orderId={}", order_id));
+        } else {
+            url.push_str(&format!(
+                "&clientId={}",
+                client_id.ok_or_else(|| Error::InvalidRequest(
+                    "either order_id or client_id is required".to_string()
+                ))?
+            ));
+        }
+        let res = self.get(url)?;
+        res.json().map_err(Into::into)
+    }
+
+    pub  fn execute_order(&self, payload: ExecuteOrderPayload) -> Result<Order> {
+        let endpoint = format!("{}/api/v1/order", self.base_url);
+        let res = self.post(endpoint.clone(), payload.clone())?;
+        let body_content = res.text()?;
+        println!("Response body: {}", body_content);
+
+        let res = self.post(endpoint, payload)?;
+
+        res.json().map_err(Into::into)
+    }
+
+    pub  fn cancel_order(
+        &self,
+        symbol: &str,
+        order_id: Option<&str>,
+        client_id: Option<u32>,
+    ) -> Result<Order> {
+        let url = format!("{}/api/v1/order", self.base_url);
+        let payload = CancelOrderPayload {
+            symbol: symbol.to_string(),
+            order_id: order_id.map(|s| s.to_string()),
+            client_id,
+        };
+
+        let res = self.delete(url, payload)?;
+        res.json().map_err(Into::into)
+    }
+
+    pub  fn get_open_orders(&self, symbol: Option<&str>) -> Result<Vec<Order>> {
+        let mut url = format!("{}/api/v1/orders", self.base_url);
+        if let Some(s) = symbol {
+            url.push_str(&format!("?symbol={s}"));
+        }
+        let res = self.get(url)?;
+        res.json().map_err(Into::into)
+    }
+
+    pub  fn cancel_open_orders(&self, payload: CancelOpenOrdersPayload) -> Result<Vec<Order>> {
+        let url = format!("{}/api/v1/orders", self.base_url);
+        let res = self.delete(url, payload)?;
+        res.json().map_err(Into::into)
+    }
+}
